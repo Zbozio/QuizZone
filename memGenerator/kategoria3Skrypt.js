55b8e7f9ddd040f8e7cf61cef5314ec32a22c87d
Bozio <karollos.bozek@gmail.com>
2024-03-16 19:40:53 +0100

    Category 3 fixed

--- memGenerator/kategoria3Skrypt.js
+++ memGenerator/kategoria3Skrypt.js
@@ -2,10 +2,13 @@ const buttons = document.querySelectorAll('.quizButton');
 const nextButton=document.getElementById("nextButton");
 const quizQuestion=document.getElementById("quizQuestion");
 const counterHandle=document.getElementById("counter");
+const popUp=document.getElementById("popUp");
+const score=document.getElementById("score");
 
 let answered=false;
-let currentCount=2;
+let currentCount=1;
 const totalCount=5;
+let goodAnswer=0;
 
 const Questions=["Które z poniższych miast leży nad rzeką Nile?","Które z poniższych państw nie graniczy z Morzem Śródziemnym?","Jak nazywa się największe jezioro w Afryce?","Gdzie znajduje się najwyższy szczyt Ameryki Północnej?"];
 const AnswersAll=[["Nairobi%","Kair","Lagos",'Johannesburg'],["Włochy","Hiszpania","Grecja","Niemcy"],["Jezioro Bajkał","Jezioro Wiktorii","Jezioro Tanganika","Jezioro Michigan"],[" Kolumbia Brytyjska, Kanada","Alaska, USA","Meksyk","Gwatemala"]];
@@ -22,17 +25,29 @@ function handleButtonClick(event) {
         }
     });
 
-    selectedButton.style.backgroundColor = isCorrect ? 'green' : 'red';
-    if (selectedButton.style.backgroundColor==='red')
+    selectedButton.style.backgroundColor = isCorrect ? 'rgb(76, 175, 80)' : 'rgb(229, 57, 53)';
+    if (selectedButton.style.backgroundColor==='rgb(229, 57, 53)')
     {
         buttons.forEach(button=>{
             if(answers_good.includes(button.innerText))
-            button.style.backgroundColor = 'green';
-            
+            {
+            button.style.backgroundColor = 'rgb(76, 175, 80)';
+            goodAnswer++;
+            }
         })
     }
     answered=true;
-    nextButton.style.display='inline-block';
+    currentCount++;
+    console.log(currentCount);
+    if(currentCount<=5) nextButton.style.display='inline-block';
+
+    if(currentCount>=6){
+        score.textContent= "Twój wynik to: " +(totalCount-goodAnswer) +"/"+ totalCount;
+        popUp.classList.add("open")
+        nextButton.style.display="hide";
+         
+
+  }
 
 }}
 
@@ -59,7 +74,7 @@ nextButton.addEventListener('click',()=>{
         })
         quizQuestion.innerText=Questions[j];
         j++;
-        currentCount++;
+        
     }
      
     
