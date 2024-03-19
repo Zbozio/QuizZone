4d9d7a85821e17ae9239de1f5b06f966159b5aff
Tomasz Działowy <karollos.bozek@gmail.com>
2024-03-19 21:00:23 +0100

    Typo :)

--- memGenerator/kategoria3Skrypt.js
+++ memGenerator/kategoria3Skrypt.js
@@ -11,7 +11,7 @@ const totalCount=5;
 let goodAnswer=0;
 
 const Questions=["Które z poniższych miast leży nad rzeką Nile?","Które z poniższych państw nie graniczy z Morzem Śródziemnym?","Jak nazywa się największe jezioro w Afryce?","Gdzie znajduje się najwyższy szczyt Ameryki Północnej?"];
-const AnswersAll=[["Nairobi%","Kair","Lagos",'Johannesburg'],["Włochy","Hiszpania","Grecja","Niemcy"],["Jezioro Bajkał","Jezioro Wiktorii","Jezioro Tanganika","Jezioro Michigan"],[" Kolumbia Brytyjska, Kanada","Alaska, USA","Meksyk","Gwatemala"]];
+const AnswersAll=[["Nairobi","Kair","Lagos",'Johannesburg'],["Włochy","Hiszpania","Grecja","Niemcy"],["Jezioro Bajkał","Jezioro Wiktorii","Jezioro Tanganika","Jezioro Michigan"],[" Kolumbia Brytyjska, Kanada","Alaska, USA","Meksyk","Gwatemala"]];
 let j=0;
 const answers_good=["Grenlandia","Kair","Niemcy","Jezioro Wiktorii","Alaska, USA"]
 
